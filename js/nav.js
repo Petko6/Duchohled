a6d88fab91c860f9d9e17bbd6fcbbb56b239b90b
Petr Procházka <petrprochazka06@email.cz>
2025-04-20 14:51:20 +0200

    block scroll inside mobile nav

--- js/nav.js
+++ js/nav.js
@@ -6,6 +6,8 @@ const mobileMenu = document.getElementById("mobilenav");
 hamburger.addEventListener("click", () => {
   mobileMenu.classList.toggle("active"); /* Shows/hides mobile menu */
   hamburger.classList.toggle("active"); /* Updates hamburger icon state */
+  /* Hides body element overflow to block scrolling while inside mobile nav menu */
+  document.body.classList.toggle("hidebodyoverflow");
 });
 
 /* Closes mobile menu when any link inside it is clicked */
@@ -13,5 +15,7 @@ mobileMenu.querySelectorAll("a").forEach((link) => {
   link.addEventListener("click", () => {
     hamburger.classList.remove("active"); /* Resets hamburger icon */
     mobileMenu.classList.remove("active"); /* Hides mobile menu */
+    /* Show overflow to enable scrolling after menu closes */
+    document.body.classList.toggle("hidebodyoverflow");
   });
 });
